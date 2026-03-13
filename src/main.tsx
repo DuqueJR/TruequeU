61c58dc82b99ef8d72230adf2112f5af30b93d69
DuqueJR <duqueruizjeronimo@gmail.com>
2026-03-13 18:36:17 -0500

    Comentarios en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react' //Detectar problemas durante el desarrollo
+import { createRoot } from 'react-dom/client' //función que conecta React con el DOM del navegador.
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from "react-router-dom" //hABILITA EL SISTEMA DE RUATAS DE REACT
 
+//Renderiza el index.html
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+  <StrictMode>  
+      <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </StrictMode>,
 )
+//<BrowserRouter> Activa el sistema de navegacion basado en rutas </BrowserRouter>
