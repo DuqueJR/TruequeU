b8d056d8dda1ebdd3715ad626997a521ba3294c0
DuqueJR <duqueruizjeronimo@gmail.com>
2026-03-13 17:32:04 -0500

    Users created and listing corrected

---
+++ src/data/users.ts
@@ -0,0 +1,54 @@
+import type {User} from "../types";
+
+export const users: User[] = [
+  {
+    id: "u1",
+    email: "juan.perez@eia.edu.co",
+    name: "Juan Pérez",
+  },
+  {
+    id: "u2",
+    email: "laura.gomez@eia.edu.co",
+    name: "Laura Gómez",
+  },
+  {
+    id: "u3",
+    email: "carlos.rodriguez@eia.edu.co",
+    name: "Carlos Rodríguez",
+  },
+  {
+    id: "u4",
+    email: "andrea.martinez@eia.edu.co",
+    name: "Andrea Martínez",
+  },
+  {
+    id: "u5",
+    email: "sebastian.lopez@eia.edu.co",
+    name: "Sebastián López",
+  },
+  {
+    id: "u6",
+    email: "camila.ramirez@eia.edu.co",
+    name: "Camila Ramírez",
+  },
+  {
+    id: "u7",
+    email: "daniel.torres@eia.edu.co",
+    name: "Daniel Torres",
+  },
+  {
+    id: "u8",
+    email: "valentina.castro@eia.edu.co",
+    name: "Valentina Castro",
+  },
+  {
+    id: "u9",
+    email: "felipe.herrera@eia.edu.co",
+    name: "Felipe Herrera",
+  },
+  {
+    id: "u10",
+    email: "sofia.moreno@eia.edu.co",
+    name: "Sofía Moreno",
+  }
+];
