9b7d0b2544e2fa09b7f41499067eedc5a8c2ae57
DuqueJR <duqueruizjeronimo@gmail.com>
2026-03-13 19:25:17 -0500

    Logica detras del search y del listing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,40 @@
 import { useStore } from "../store/useStore"
+import { useState } from "react"
 
 export default function HomePage() {
 
-  const user = useStore((state) => state.user)!
+  const listings = useStore((state) => state.listings)
 
-  
+  const [search, setSearch] = useState("")  //setSearch actualia la busqueda - seState es un hook de React que permite a un componente guardar y actualizar estado interno
+
+  //la función que React te da para actualizar el estado creado con useState.
+
+  const filteredListings = listings.filter((item) =>
+    item.title.toLowerCase().includes(search.toLowerCase())
+  )
 
   return (
-    <main className='mx-auto max-w-6xl px-6 py-6'>
-      <div className="flex flex-col gap-2">
-        <h1 className="m-0 text-2xl font-semibold">
-          Welcome back, {user.name}!
-        </h1>
-      </div>
-    </main>
+
+    <div>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      {filteredListings.map((item) => (
+
+        <div key={item.id}>
+
+          <h3>{item.title}</h3>
+          <p>${item.price}</p>
+
+        </div>
+
+      ))}
+
+    </div>
   )
 }
